ef63c7b647abce409fa2a4e921cc293b2bb91485
Vinicio Chavarría <vichavarria21@gmail.com>
2021-02-28 08:07:12 -0600

    Se genero una secuencia aleatoria con un new array | se guardaron datos en la class Juego

--- juego_simon_dice/js/javascript.js
+++ juego_simon_dice/js/javascript.js
@@ -7,14 +7,26 @@ const btnEmpezar = document.getElementById('btnEmpezar')
 class Juego {
     constructor() {
         this.inicializar()
+        this.generarSecuencia()
     }
 
     inicializar() {
         btnEmpezar.classList.add('hide')
+        this.nivel = 1/* A medida que avance el nivel sube */
+        this.colores = {
+            celeste : celeste,
+            violeta : violeta,
+            naranja : naranja,
+            verde : verde
+        } /* Guarde este objeto aqui que hace referencia a las const de arriba para usarlas mas fácil */
     }
+    generarSecuencia(){/* math.random da un valor entre cero y uno | math.floor redondea para abajo el numero */
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4)) 
+    } /* .fill() define que valor sera el que tenga el array */
 }
 
 
 function empezarJuego () {
-    var juego = new Juego()
+    window.juego = new Juego()
 }
+
